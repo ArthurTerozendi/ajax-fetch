b09a650588aba7321b016805ac3a9cdc38075625
Arthur Terozendi <arthurterozendi@gmail.com>
2021-04-03 12:27:38 -0300

    ajax exerc 03

--- server/servidor_imagens.js
+++ server/servidor_imagens.js
@@ -18,4 +18,25 @@ app.get('/imagens', (req, res, next) => {
     }
 });
 
+let sim = 0;
+let nao = 0;
+
+app.get('/contador', (req, res, next) => {
+    res.send({'sim': sim, 'nao': nao});
+});
+
+app.post('/contador', (req, res, next) => {
+    if (req.body.operacao == 'sim') {
+        sim++;
+    }
+    else if (req.body.operacao == 'nao'){
+        nao++;
+    }
+    else {
+        res.sendStatus(400).send("Operação Invalida");
+    }
+    res.send({'sim': sim, 'nao': nao});
+});
+
+
 const img = ['../server/img/garfield1.jpg', '../server/img/garfield2.png', '../server/img/garfield3.gif', '../server/img/garfield4.jpg', '../server/img/garfield8.png', '../server/img/garfield20.jpg']
